3aedd2b7058c3172575ac3a932eeed0f98f7fdc0
DumbDev343 <62670550+dumbdev343@users.noreply.github.com>
2026-06-02 17:39:50 +1200

    Fix macOS/Darwin error message and add updates for linux

--- main.py
+++ main.py
@@ -34,6 +34,8 @@ print(f"The drive you are running this script of has {shutil.disk_usage(os.getcw
 if platform.system() == ("Linux"):
     if os.path.exists("/etc/debian_version"):
         print("You are running on the distro Debian!")
+    if os.path.exists("/etc/arch-release"):
+        print("You are running on an Arch-based Distro!")
 print(" ")
 print(" ")
 
@@ -43,6 +45,8 @@ if "yes" in pkgyes:
     if platform.system() == ("Windows"):
         print("Running on Windows, using winget....")
         os.system(f"winget install {pkgname}")
+    if platform.system == "Darwin":
+        print("Sorry, Darwin/macOS is unsupported for the time being (Updates and Installing Packages)")
     if platform.system() == ("Linux"):
         if os.path.exists("/etc/apt/apt.conf.d"):
             print("APT detected, using that instead....")
@@ -64,3 +68,26 @@ if "yes" in pkgyes:
         if os.path.exists("/etc/zypp/zypper.conf"):
             print("Running with zypper....")
             os.system(f"sudo zypper install {pkgname}")
+updyes = input("Would you like to update your computer?")
+if "yes" in updyes:
+    if platform.system() == "Linux":
+        if os.path.exists("/etc/apt/apt.conf.d"):
+            print("Running on APT. ")
+            os.system("sudo apt update && apt upgrade -y ")
+        if os.path.exists("/etc/pacman.conf"):
+            print("Pacman detected.")
+            os.system("sudo pacman -Syu")
+        if os.path.exists("/etc/dnf/dnf.conf"):
+            print("Using DNF.")
+            os.system("sudo dnf upgrade --refresh")
+        if os.path.exists("/data/data/com.termux/files/usr"):
+            print("Using pkg/apt.")
+            os.system("pkg update && pkg upgrade -y")
+        if os.path.exists("/etc/pkg"):
+            print("Using pkg to update...")
+            os.system("pkg update && pkg upgrade -y")
+        if os.path.exists("/etc/zypp/zypper.conf"):
+            os.system("sudo zypper dup -y")
+    if platform.system() == "Darwin":
+        print("Sorry, Darwin/macOS is unsupported for the time being (Updates and Installing Packages)")
+        
